0ffe9b8a4c2077608a4c37b200eb8c6c0bb8d8a9
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-10 09:40:47 +0000

    More than one word now

--- oriole-ferret.rb
+++ oriole-ferret.rb
@@ -1,4 +1,11 @@
-puts "Tell me about your day or whatever. Start typing..."
+# Instructions from program:
+puts "I dare you to start typing..."
+# This is what you have typed in:
 user_types = gets.chomp
+# And this is what the bastard has spat out:
+# puts user_types.gsub!(/and/, "buggeration")
+# puts user_types.gsub!(".", "?!!") # (/and/, "buggeration")
 
-puts user_types.gsub!(/and/, "buggeration")
+map = {"and" => "buggeration", "." => "?!!"}
+map.each {|k,v| user_types.gsub!(k,v)}
+puts "Here is your output:" + "\n" + user_types
