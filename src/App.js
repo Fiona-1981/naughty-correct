be32f5d1d54e13516132d12ac5369ad04a3257d2
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-06-12 16:02:03 +0100

    Added some React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+function UserMessage({onSubmitUserMessage}) {
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmitUserMessage(event.target.elements.messageInput.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Get on with it then!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="messageInput">Go on, type something...</label>
+        <input id="messageInput" type="text" />
+      </div>
+      <button type="submit">Go forth</button>
+    </form>
+  )
 }
 
-export default App;
+// Now... How to mess with the userMessage... Will use JavaScript to 
+// do an if "." return "???!!!" kind of thing. How do I fit that in? In a component file
+// probably to keep it separate from this file, but then how to import it to here.
+// Sometimes import this and that doesn't seem to work.
+// Perhaps for simplicity for now, add it to this page since it's only a very small
+// program.
+
+function App() {
+  const onSubmitUserMessage = userMessage => alert(`You wrote: ${userMessage}`)
+  return <UserMessage onSubmitUserMessage={onSubmitUserMessage} />
+}
+
+export default App
